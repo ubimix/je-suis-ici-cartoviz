83c5ce5b3087be95380d3c72e5f2c754c9095e91
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-04-02 12:01:00 +0200

    A basic data layer with interactions was added

--- index.js
+++ index.js
@@ -1,38 +1,52 @@
 (function(context) {
     $(function() {
         var mapElement = $('#map');
-        var tilesUrl = mapElement.data('tiles-url');
-        var utfgridUrl = mapElement.data('utfgrid-url');
-        var attributionElm = mapElement.find('.attribution');
-        var attribution = attributionElm.html();
+        var layers = [];
+        mapElement.find('[data-type="layer"]').each(function() {
+            var elm = $(this);
+            var tilesUrl = elm.data('tiles-url');
+            if (tilesUrl) {
+                var attributionElm = elm.find('.attribution');
+                var attribution = attributionElm.html();
+                attributionElm.remove();
+                var tilesLayer = L.tileLayer(tilesUrl, {
+                    attribution : attribution,
+                    minZoom : minZoom,
+                    maxZoom : maxZoom,
+                });
+                layers.push(tilesLayer);
+            }
+            var utfgridUrl = elm.data('utfgrid-url');
+            if (utfgridUrl) {
+                var utfGrid = new L.UtfGrid(utfgridUrl);
+                var template = _.template(elm.text());
+                var panelSelector = elm.data('panel') || '#info';
+                var action = elm.data('action') || 'mouseover';
+                utfGrid.on(action, function(ev) {
+                    var panel = $(panelSelector);
+                    var html = template(ev.data);
+                    panel.html(html);
+                    panel.show();
+                });
+                layers.push(utfGrid);
+            }
+        })
         mapElement.html('');
-        var center = mapElement.data('center');
+        var center = mapElement.data('center') || [ 0, 0 ];
+        center = [ center[1], center[0] ];
         var zoom = mapElement.data('zoom');
         var minZoom = mapElement.data('min-zoom') || 2;
         var maxZoom = mapElement.data('max-zoom') || 18;
 
-        console.log('tilesUrl:', tilesUrl);
-        console.log('utfgridUrl:', utfgridUrl);
         console.log('center:', center);
         console.log('minZoom:', minZoom);
         console.log('maxZoom:', maxZoom);
         console.log('zoom:', zoom);
 
         var map = L.map(mapElement[0]).setView(center, zoom);
-        var tiles = L.tileLayer(tilesUrl, {
-            attribution : attribution,
-            minZoom : minZoom,
-            maxZoom : maxZoom,
-        });
-        map.addLayer(tiles);
-
-        if (utfgridUrl) {
-            var utfGrid = new L.UtfGrid(utfgridUrl);
-            map.addLayer(utfGrid);
-            utfGrid.on('mouseover', function(ev) {
-                showInfo(ev.data);
-            })
-        }
+        _.each(layers, function(layer) {
+            map.addLayer(layer);
+        })
 
         map.on('click', function(e) {
             console.log(map.getZoom() + ' [' + e.latlng.lng + ','
@@ -40,16 +54,5 @@
         })
 
     });
-    function showInfo(data) {
-        var panel = $('#info');
-        console.log(panel[0])
-        panel.find('[data-placeholder]').each(function() {
-            var elm = $(this);
-            var field = elm.data('placeholder');
-            console.log(field, data[field]);
-            elm.text(data[field]);
-        })
-        panel.show();
-    }
 
 })(this);
---
+++ layers/autolib/style.mss
@@ -0,0 +1,11 @@
+#autolib {
+    marker-fill : red;
+    marker-opacity : 1;
+    marker-width : 1;
+    marker-line-color : red;
+    marker-line-width : 1;
+    marker-line-opacity : 1;
+    marker-placement : point;
+    marker-type : ellipse;
+    marker-allow-overlap : true;
+}
