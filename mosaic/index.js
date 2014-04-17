a417e303a92f04d5593ac763ebfbad2e6347e068
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-04-17 17:03:43 +0200

    A Mosaic-based implementation was added

---
+++ mosaic/index.js
@@ -0,0 +1,36 @@
+(function(context) {
+    var P = Q;
+
+    $(initialize);
+
+    var appConfig = {
+        templatesSelector : '#templates',
+        mapSelector : '#map',
+        datalayersSelector : '#map [data-map-layer]'
+    }
+
+    function initialize() {
+        var application = new Mosaic.AppConfigurator(appConfig);
+        loadJson('./data/data.json').then(function(data) {
+            application.addGeoJsonDataSet(data, {
+                clusterPoints : true,
+            });
+        }).then(function() {
+            application.start();
+        }).done();
+    }
+
+    /**
+     * Return a promise for the data loaded from the specified URL
+     */
+    function loadJson(url) {
+        var deferred = P.defer();
+        $.get(url, function(data) {
+            deferred.resolve(data);
+        }).fail(function(error) {
+            deferred.reject(error);
+        });
+        return deferred.promise;
+    }
+
+})(this);
