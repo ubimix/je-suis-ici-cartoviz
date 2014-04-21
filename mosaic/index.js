667b6d58d3276a9cb92a84c3f4cc9519e6f0af84
kotelnikov <mikhail.kotelnikov@ubimix.com>
2014-04-21 15:18:43 +0200

    Template changes

--- mosaic/index.js
+++ mosaic/index.js
@@ -1,36 +1,11 @@
-(function(context) {
-    var P = Q;
-
-    $(initialize);
-
+$(function() {
     var appConfig = {
         templatesSelector : '#templates',
         mapSelector : '#map',
+        listSelector : '#list',
         datalayersSelector : '#map [data-map-layer]'
     }
 
-    function initialize() {
-        var application = new Mosaic.AppConfigurator(appConfig);
-        loadJson('./data/data.json').then(function(data) {
-            application.addGeoJsonDataSet(data, {
-                clusterPoints : true,
-            });
-        }).then(function() {
-            application.start();
-        }).done();
-    }
-
-    /**
-     * Return a promise for the data loaded from the specified URL
-     */
-    function loadJson(url) {
-        var deferred = Mosaic.Promise.defer();
-        $.get(url, function(data) {
-            deferred.resolve(data);
-        }).fail(function(error) {
-            deferred.reject(error);
-        });
-        return deferred.promise;
-    }
-
-})(this);
+    var application = new Mosaic.AppConfigurator(appConfig);
+    application.start();
+});
